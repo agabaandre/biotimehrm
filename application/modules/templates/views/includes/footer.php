aa908400dddcd49966a9a1b7766f8d1b8eaacf20
Agaba Andrew <agabaandre@gmail.com>
2021-06-18 15:33:53 +0300

    finished datables

--- application/modules/templates/views/includes/footer.php
+++ application/modules/templates/views/includes/footer.php
@@ -45,12 +45,6 @@
  <script src="<?php echo base_url(); ?>assets/js/notify.min.js"></script>
  <script>
     $(function () {
-//     $("#mytab2").DataTable({
-//       "responsive": true, "lengthChange": false, "autoWidth": false,
-//       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-//     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-//   });
-  // $(function () {
     
   
     $('#mytab2').DataTable({
