a962adae566a17490d7e6841a7b13eb429692953
Agaba Andrew <agabaandre@gmail.com>
2021-07-21 17:30:25 +0300

    gi

--- application/modules/dashboard/views/home.php
+++ application/modules/dashboard/views/home.php
@@ -500,91 +500,91 @@
  //duty roster graph
 <?php 
 
-$graph=Modules::run("reports/dutygraphData"); 
+// $graph=Modules::run("reports/dutygraphData"); 
 
- ?> 
- Highcharts.chart('line_graph', {
-     chart: {
-         type: 'line'
-     },
-      title: {
-         text: 'Duty Roster Reporting Rate'
-     },
-     subtitle: {
-         text: ''
-     },
-     xAxis: {
-         categories: <?php echo json_encode($graph['period']); ?>
-     },
-     yAxis: {
-         title: {
-             text: 'Staff'
-         }
-     },
-     plotOptions: {
-         line: {
-             dataLabels: {
-                 enabled: true
-             },
-             enableMouseTracking: true
-         }
-     },
-     credits: {
-             enabled: false
-     },
-     series: [{
-         name: 'Staff',
-         data: <?php echo json_encode($graph['data'],JSON_NUMERIC_CHECK); ?>
-     }, {
-         name: 'Target',
-         data: <?php echo json_encode($graph['target'],JSON_NUMERIC_CHECK); ?>
-     }]
- });
+//  ?> 
+//  Highcharts.chart('line_graph', {
+//      chart: {
+//          type: 'line'
+//      },
+//       title: {
+//          text: 'Duty Roster Reporting Rate'
+//      },
+//      subtitle: {
+//          text: ''
+//      },
+//      xAxis: {
+//          categories: <?php echo json_encode($graph['period']); ?>
+//      },
+//      yAxis: {
+//          title: {
+//              text: 'Staff'
+//          }
+//      },
+//      plotOptions: {
+//          line: {
+//              dataLabels: {
+//                  enabled: true
+//              },
+//              enableMouseTracking: true
+//          }
+//      },
+//      credits: {
+//              enabled: false
+//      },
+//      series: [{
+//          name: 'Staff',
+//          data: <?php echo json_encode($graph['data'],JSON_NUMERIC_CHECK); ?>
+//      }, {
+//          name: 'Target',
+//          data: <?php echo json_encode($graph['target'],JSON_NUMERIC_CHECK); ?>
+//      }]
+//  });
  
 
-<?php 
+// <?php 
 
-$graph=Modules::run("reports/graphData"); 
+// $graph=Modules::run("reports/graphData"); 
 
-?> 
+// ?> 
 
-Highcharts.chart('line_graph_att', {
-    chart: {
-        type: 'line'
-    },
-    title: {
-        text: 'Attendace Reporting Rate'
-    },
-    subtitle: {
-        text: ''
-    },
-    xAxis: {
-        categories: <?php echo json_encode($graph['period']); ?>
-    },
-    yAxis: {
-        title: {
-            text: 'Staff'
-        }
-    },
-    plotOptions: {
-        line: {
-            dataLabels: {
-                enabled: true
-            },
-            enableMouseTracking: true
-        }
-    },
-    credits: {
-            enabled: false
-    },
-    series: [{
-        name: 'Staff',
-        data: <?php echo json_encode($graph['data'],JSON_NUMERIC_CHECK); ?>
-    }, {
-        name: 'Target',
-        data: <?php echo json_encode($graph['target'],JSON_NUMERIC_CHECK); ?>
-    }]
-});
+// Highcharts.chart('line_graph_att', {
+//     chart: {
+//         type: 'line'
+//     },
+//     title: {
+//         text: 'Attendace Reporting Rate'
+//     },
+//     subtitle: {
+//         text: ''
+//     },
+//     xAxis: {
+//         categories: <?php echo json_encode($graph['period']); ?>
+//     },
+//     yAxis: {
+//         title: {
+//             text: 'Staff'
+//         }
+//     },
+//     plotOptions: {
+//         line: {
+//             dataLabels: {
+//                 enabled: true
+//             },
+//             enableMouseTracking: true
+//         }
+//     },
+//     credits: {
+//             enabled: false
+//     },
+//     series: [{
+//         name: 'Staff',
+//         data: <?php echo json_encode($graph['data'],JSON_NUMERIC_CHECK); ?>
+//     }, {
+//         name: 'Target',
+//         data: <?php echo json_encode($graph['target'],JSON_NUMERIC_CHECK); ?>
+//     }]
+// });
 
 </script>
 
