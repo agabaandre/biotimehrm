87c5e85cb3db611e79bf551acba101a4fc1a6170
Agaba Andrew <agabaandre@gmail.com>
2021-08-02 10:43:12 +0300

    rosta

--- application/modules/employees/views/print_individual_time_logs.php
+++ application/modules/employees/views/print_individual_time_logs.php
@@ -145,7 +145,7 @@ table.minimalistBlack tfoot td {
                                 <tr>
                                     <td><?php echo $wdays; ?></td>
                                     <td><?php echo date('j F,Y', strtotime($timelog->date)); ?></td>
-                                    <td><?php echo $timelog->time_in; ?></td>
+                                    <td><?php echo date('H:i:s', strtotime($timelog->time_in)); ?></td>
                                     <td><?php if (!empty($time_out=$timelog->time_out)) { echo date('H:i:s', strtotime($time_out=$timelog->time_out));} ?></td>
                                     <td>
                                     <?php
