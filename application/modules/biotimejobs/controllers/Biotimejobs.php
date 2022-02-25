d1833e7482c0fa63555cfd741ac4f1a1d077f806
Agaba Andrew <agabaandre@gmail.com>
2022-02-25 15:33:39 +0300

    biotime night

--- application/modules/biotimejobs/controllers/Biotimejobs.php
+++ application/modules/biotimejobs/controllers/Biotimejobs.php
@@ -634,13 +634,13 @@ public function biotimeFacilities()
     $today = date('Y-m-d');
     $yesterday = date("Y-m-d", strtotime("-1 day"));
 
-    $nights=$this->db->query("SELECT duty_date,ihris_pid,entry_id from duty_rosta where schedule_id='14'  and concat(duty_date,ihris_pid) in (SELECT entry_id from clk_log WHERE date='$yesterday'
+    $nights=$this->db->query("SELECT duty_date,ihris_pid,entry_id,card_number from duty_rosta,ihrisdata where schedule_id='14' and ihrisdata.ihris_pid=duty_rosta.ihris_pid  and concat(duty_date,ihris_pid) in (SELECT entry_id from clk_log WHERE date='$yesterday'
     )")->result();
     foreach($nights as $night):
 
         $nights=$night->duty_date.$night->ihris_pid;
         
-        // $query=$this->db->query("SELECT punch_time from biotime_data,ihrisdata where (biotime_data.emp_code=ihrisdata.card_number or biotime_data.ihris_pid=ihrisdata.ihris_pid) AND (punch_state='1' OR punch_state='Check Out' OR punch_state='0') AND concat(DATE(biotime_data.punch_time),ihrisdata.ihris_pid) = '$nights' ");
+         //$query=$this->db->query("SELECT punch_time from biotime_data_history,ihrisdata where (biotime_data_history.emp_code='$night->card_number') AND (punch_state='1' OR punch_state='Check Out' OR punch_state='0') AND DATE(biotime_data_history.punch_time)='$today' ");
         // $entry_id=$query->result();
     
         //  foreach($entry_id as $entry){
