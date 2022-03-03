18359cd9aa0646a37f48e58c8f0bfe2bfcfa8558
Agaba Andrew <agabaandre@gmail.com>
2022-03-03 13:26:39 +0300

    absent helper

--- application/modules/attendance/views/summary_pdf.php
+++ application/modules/attendance/views/summary_pdf.php
@@ -81,7 +81,93 @@ td{
 </head>
 <body>
 
-<p>tHIS IS DATA</p>
+
+<table class="items" style="font-size: 12pt; border-collapse: collapse; " cellpadding="8" width="100%">
+
+           
+                    <tr style="border-right: 0; border-left: 0; border-top: 0;">
+	<td colspan=2 style="border-right: 0; border-left: 0; border-top: 0;"><img src="<?php echo base_url(); ?>assets/img/MOH.png" width="100px"></td>
+
+	<td colspan=4 style="border-right: 0; border-left: 0; border-top: 0;">
+		<h4>
+<?php 
+
+
+?>
+	<h4">
+
+		<?php
+		 echo 'MONTHLY ATTENDANCE TO DUTY SUMMARY FOR'. $_SESSION['facility_name']." ".date('F, Y',strtotime($dates));
+		?>
+	</h4>
+
+
+
+	</h4></td>
+</tr>
+           
+  <!-- b -->
+
+<tr>
+	<th>#</th>
+	<th>Name</th>
+	<th>Job</th>
+	<th>Present</th>
+	<th>Off Duty</th>
+	<th>Official Request</th>
+	<th>Leave</th>
+	<th>Holiday</th>
+	<th>Absent</th>
+	<th>Day Schedule</th>
+	<th>Evening Schedule</th>
+	<th> Night Schedule</th>
+	<th> Present %</th>
+	
+
+
+</tr>
+
+<tbody>
+
+
+<?php 
+
+$no=(!empty($this->uri->segment(3)))?$this->uri->segment(3):0;
+foreach($sums as $sum):
+//print_r($sums); 
+?>
+
+<tr>
+	<td data-label="no"><?php echo $no;?></td>
+	<td data-label="Name"><?php echo $sum['fullname'].' '.$sum['othername'];?></td>
+	<td data-label="Job"><?php echo $sum['job']?></td>
+    <td data-label="P"><?php if(!empty($present=$sum['P'])){ echo $present; } else{ echo 0; }?></td>
+    <td data-label="O"><?php if(!empty($O=$sum['O'])){ echo $O; } else{ echo 0; }?></td>
+	<td data-label="R"><?php if(!empty($R=$sum['R'])){ echo $R; } else{ echo 0; }?></td>
+	<td data-label="L"><?php if(!empty($L=$sum['L'])){ echo $L; } else{ echo 0; }?></td>
+	<td data-label="H"><?php if(!empty($H=$sum['H'])){ echo $H; } else{ echo 0; }?></td>
+	<td data-label="A"></td>
+	<td data-label="D"><?php $roster=Modules::run('attendance/attrosta',$dates,urlencode($sum['ihris_pid'])); ?><?php echo $day=$roster['Day'][0]->days; ?></td>
+	<td data-label="E"><?php echo $eve=$roster['Evening'][0]->days; ?></td>
+	<td data-label="N"><?php echo $night=$roster['Night'][0]->days;?></td>
+	<td data-label="Percentage Pr"><?php $per= round(($present/($day+$night+$eve))*100,1); if(is_infinite($per)||is_nan($per)){ echo  0; } else{ echo $per; } ?> % </td>	
+</tr>
+
+<?php
+
+$no++; 
+
+endforeach; ?>
+
+
+
+</tbody>
+
+
+
+
+
+</table>
 
 </body>
 </html>
