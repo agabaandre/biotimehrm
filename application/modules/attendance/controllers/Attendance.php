37209a7fd276a93e328f71ec64cdcb41d5925b77
Agaba Andrew <agabaandre@gmail.com>
2022-03-03 13:46:24 +0300

    absent helper

--- application/modules/attendance/controllers/Attendance.php
+++ application/modules/attendance/controllers/Attendance.php
@@ -156,7 +156,7 @@ class Attendance extends MX_Controller {
         $eve=$roster['Evening'][0]->days;
 		$day=$roster['Day'][0]->days;
 		$night=$roster['Night'][0]->days;
-		$r_days=($eve+$night+$night);
+		$r_days=($eve+$day+$night);
 		$absent=days_absent_helper($present,$r_days);
 		$per= round(($present/($day+$night+$eve))*100,1); if(is_infinite($per)||is_nan($per)){ $per = 0; } else{  $per; }
         $days =array("Name"=>$data['fullname'],"Job"=>$data['job'],"Department"=>$data['department'], "Present"=>$present, "Off
