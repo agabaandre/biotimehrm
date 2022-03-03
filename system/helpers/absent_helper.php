55adab3e250b6c73803614d39479ffacc68c5c9c
Agaba Andrew <agabaandre@gmail.com>
2022-03-03 11:31:00 +0300

    dutyreport

--- system/helpers/absent_helper.php
+++ system/helpers/absent_helper.php
@@ -3,8 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 *Developer:Agaba Andrew 2022
-* Helps to filter dashboard data by financial year or by the user department
-*andyear,whereyear,andsubject,wheresubject
+
 */
 if (!function_exists('days_absent_helper')) {
     function days_absent_helper($present,$scheduled){
