57aba19dadfcdd542938ae3abe102b2c540865e0
AGABA ANDREW <agabaandre@gmail.com>
2022-09-16 21:06:18 +0300

    Auth users

--- application/modules/employees/views/staff.php
+++ application/modules/employees/views/staff.php
@@ -6,49 +6,50 @@
       <section class="col-lg-12 ">
         <?php $staffs = Modules::run('employees/get_employees');
         ?>
-        <table id="mytab2" class="table table-bordered table-striped mytable">
-          <thead>
-            <tr>
-              <th>Staff iHRIS ID</th>
-              <th>NIN</th>
-              <th>Name</th>
-              <th>Gender</th>
-              <th>Birth Date</th>
-              <th>Phone</th>
-              <th>Facility</th>
-              <th>Department</th>
-              <th>Job</th>
-              <th>Employment Terms</th>
-              <th>Card Number</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php $i = 1;
-            foreach ($staffs as $staff) {
-            ?>
+        <h4>Staff List - <?php echo $_SESSION['facility_name']; ?></h4>
+          <table id="mytab2" class="table table-bordered table-striped mytable">
+            <thead>
               <tr>
-                <td data-label="Staff iHRIS ID" style="background-image:url('https://cdn-icons-png.flaticon.com/512/180/180656.png'); width:15px !imporntant; height:15px !imporntant; background-size: contain;background-position: center center;background-repeat: no-repeat; font-weight:bold; color:grey; font-size:13px; margin:0 auto; "><?php echo str_replace('person|', '', $staff->ihris_pid); ?></td>
-                <td data-label=" NATIONAL ID NUMBER"><?php echo $staff->nin; ?></td>
-                <td data-label="NAME"><?php echo $staff->surname . " " . $staff->firstname . " " . $staff->othername; ?>
-                </td>
-                <td data-label="GENDER"><?php echo $staff->gender; ?></td>
-                <td data-label="DATE OF BIRTH"><?php echo $staff->birth_date; ?></td>
-                <td data-label="TELEPHONE"><?php if (empty($staff->mobile)) {
-                                              echo $staff->mobile;
-                                            } else {
-                                              echo $staff->telephone;
-                                            } ?></td>
-                <td data-label="FACILITY"><?php echo $staff->facility; ?></td>
-                <td data-label="DEPARTMENT"><?php echo $staff->department; ?></td>
-                <td data-label="JOB"><?php echo $staff->job; ?></td>
-                <td data-label="TERMS"><?php echo str_replace("CContract", "Central Contract", str_replace("LContract", "Local Contract", str_replace("employment_terms|", "", $staff->employment_terms))); ?></td>
-                <td data-label="CARD NUMBER"><?php echo $staff->card_number; ?></td>
+                <th>Staff iHRIS ID</th>
+                <th>NIN</th>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Birth Date</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Department</th>
+                <th>Job</th>
+                <th>Employment Terms</th>
+                <th>Card Number</th>
               </tr>
-            <?php   } ?>
-          </tbody>
-          <tfoot>
-          </tfoot>
-        </table>
+            </thead>
+            <tbody>
+              <?php $i = 1;
+              foreach ($staffs as $staff) {
+              ?>
+                <tr>
+                  <td data-label="Staff iHRIS ID" style="background-image:url('https://cdn-icons-png.flaticon.com/512/180/180656.png'); width:15px !imporntant; height:15px !imporntant; background-size: contain;background-position: center center;background-repeat: no-repeat; font-weight:bold; color:grey; font-size:13px; margin:0 auto; "><?php echo str_replace('person|', '', $staff->ihris_pid); ?></td>
+                  <td data-label=" NATIONAL ID NUMBER"><?php echo $staff->nin; ?></td>
+                  <td data-label="NAME"><?php echo $staff->surname . " " . $staff->firstname . " " . $staff->othername; ?>
+                  </td>
+                  <td data-label="GENDER"><?php echo $staff->gender; ?></td>
+                  <td data-label="DATE OF BIRTH"><?php echo $staff->birth_date; ?></td>
+                  <td data-label="TELEPHONE"><?php if (empty($staff->mobile)) {
+                                                echo $staff->mobile;
+                                              } else {
+                                                echo $staff->telephone;
+                                              } ?></td>
+                  <td data-label="FACILITY"><?php echo $staff->email; ?></td>
+                  <td data-label="DEPARTMENT"><?php echo $staff->department; ?></td>
+                  <td data-label="JOB"><?php echo $staff->job; ?></td>
+                  <td data-label="TERMS"><?php echo str_replace("CContract", "Central Contract", str_replace("LContract", "Local Contract", str_replace("employment_terms|", "", $staff->employment_terms))); ?></td>
+                  <td data-label="CARD NUMBER"><?php echo $staff->card_number; ?></td>
+                </tr>
+              <?php   } ?>
+            </tbody>
+            <tfoot>
+            </tfoot>
+          </table>
       </section>
     </div>
     <!-- /.row (main row) -->
