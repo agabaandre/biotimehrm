89e355ef1074dc3c0174f25153431bf0caac34d1
AGABA ANDREW <agabaandre@gmail.com>
2022-09-18 17:56:29 +0300

    clean files

--- application/modules/rosta/views/duty_report.php
+++ application/modules/rosta/views/duty_report.php
@@ -25,177 +25,182 @@ function dayState($day, $scheduled)
 if (count($duties) > 0) {
 ?>
 <?php } ?>
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="panel panel-default">
-				<div class="panel-body" style="overflow-x: scroll;">
-					<div class="callout callout-success">
-						<form class="form-horizontal" style="padding-bottom: 2em;" action="<?php echo base_url(); ?>rosta/fetch_report" method="post">
-							<div class="row">
-								<div class="col-md-3">
-									<div class="control-group">
-										<input type="hidden" id="month" value="<?php echo $month; ?>">
-										<select class="form-control select2" name="month" onchange="this.form.submit()">
-											<option value="<?php echo $month; ?>"><?php echo strtoupper(date('F', mktime(0, 0, 0, $month, 10))) . "(Showing below)"; ?></option>
-											<option value="01">JANUARY</option>
-											<option value="02">FEBRUARY</option>
-											<option value="03">MARCH</option>
-											<option value="04">APRIL</option>
-											<option value="05">MAY</option>
-											<option value="06">JUNE</option>
-											<option value="07">JULY</option>
-											<option value="08">AUGUST</option>
-											<option value="09">SEPTEMBER</option>
-											<option value="10">OCTOBER</option>
-											<option value="11">NOVEMBER</option>
-											<option value="12">DECEMBER</option>
-										</select>
+<div class="card">
+	<div class="">
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<div class="panel-body" style="overflow-x: scroll;">
+						<div class="callout callout-success">
+							<form class="form-horizontal" style="padding-bottom: 2em;" action="<?php echo base_url(); ?>rosta/fetch_report" method="post">
+								<div class="row">
+									<div class="col-md-3">
+										<div class="control-group">
+											<input type="hidden" id="month" value="<?php echo $month; ?>">
+											<select class="form-control select2" name="month" onchange="this.form.submit()">
+												<option value="<?php echo $month; ?>"><?php echo strtoupper(date('F', mktime(0, 0, 0, $month, 10))) . "(Showing below)"; ?></option>
+												<option value="01">JANUARY</option>
+												<option value="02">FEBRUARY</option>
+												<option value="03">MARCH</option>
+												<option value="04">APRIL</option>
+												<option value="05">MAY</option>
+												<option value="06">JUNE</option>
+												<option value="07">JULY</option>
+												<option value="08">AUGUST</option>
+												<option value="09">SEPTEMBER</option>
+												<option value="10">OCTOBER</option>
+												<option value="11">NOVEMBER</option>
+												<option value="12">DECEMBER</option>
+											</select>
+										</div>
+									</div>
+									<div class="col-md-3">
+										<div class="control-group">
+											<input type="hidden" id="year" value="<?php echo $year; ?>">
+											<select class="form-control select2" name="year" onchange="this.form.submit()">
+												<option><?php echo $year; ?></option>
+												<?php for ($i = -5; $i <= 25; $i++) {  ?>
+													<option><?php echo 2017 + $i; ?></option>
+												<?php }  ?>
+											</select>
+										</div>
+									</div>
+									<div class="col-md-3">
+										<div class="control-group">
+											<?php
+											$facility = $this->session->userdata['facility'];
+											//print_r($facility);
+											$employees = Modules::run("employees/get_employees"); ?>
+											<select class="form-control select2" name="empid" select2>
+												<option value="" selected disabled>Select Employee</option>
+												<?php foreach ($employees as $employee) {  ?>
+													<option value="<?php echo $employee->ihris_pid ?>"><?php echo $employee->surname . ' ' . $employee->firstname . ' ' . $employee->othername; ?></option>
+												<?php }  ?>
+											</select>
+										</div>
+									</div>
+									<div class="col-md-3">
+										<div class="control-group">
+											<button type="submit" name="" class="btn bg-gray-dark color-pale" style="font-size:12px;">Apply</button>
+											<a href="<?php echo base_url() ?>rosta/print_roster/<?php echo $year . "/" . $month; ?>" class="btn bg-gray-dark color-pale" target="_blank"><i class="fa fa-print"></i>Print</a>
+										</div>
 									</div>
 								</div>
-								<div class="col-md-3">
-									<div class="control-group">
-										<input type="hidden" id="year" value="<?php echo $year; ?>">
-										<select class="form-control select2" name="year" onchange="this.form.submit()">
-											<option><?php echo $year; ?></option>
-											<?php for ($i = -5; $i <= 25; $i++) {  ?>
-												<option><?php echo 2017 + $i; ?></option>
-											<?php }  ?>
-										</select>
-									</div>
-								</div>
-								<div class="col-md-3">
-									<div class="control-group">
-										<?php
-										$facility = $this->session->userdata['facility'];
-										//print_r($facility);
-										$employees = Modules::run("employees/get_employees"); ?>
-										<select class="form-control select2" name="empid" select2>
-											<option value="" selected disabled>Select Employee</option>
-											<?php foreach ($employees as $employee) {  ?>
-												<option value="<?php echo $employee->ihris_pid ?>"><?php echo $employee->surname . ' ' . $employee->firstname . ' ' . $employee->othername; ?></option>
-											<?php }  ?>
-										</select>
-									</div>
-								</div>
-								<div class="col-md-3">
-									<div class="control-group">
-										<button type="submit" name="" class="btn bg-gray-dark color-pale" style="font-size:12px;">Apply</button>
-										<a href="<?php echo base_url() ?>rosta/print_roster/<?php echo $year . "/" . $month; ?>" class="btn bg-gray-dark color-pale" target="_blank"><i class="fa fa-print"></i>Print</a>
-									</div>
-								</div>
+							</form>
+						</div>
+						<div class="callout callout-success">
+							<p class="" style="text-align: center; margin-top: 5px; font-weight:bold; font-size: 1rem;"> Duty Roster Key</p>
+							<hr style="color:#15b178;">
+							<?php $colors = Modules::run('schedules/getrosterKey'); ?>
+							<div class="col-lg-12" style="text-align:center;">
+								<p style="text-align:center; font-weight:bold; font:14rem;"></p>
+								<?php foreach ($colors as $color) { ?>
+									<button type="button" class="btn btn-sm btnkey bg-gray-dark color-pale"><?php echo $color->schedule; ?> (<?php echo $color->letter; ?>)
+									</button>
+								<?php  } ?>
 							</div>
-						</form>
-					</div>
-					<div class="callout callout-success">
-						<p class="" style="text-align: center; margin-top: 5px; font-weight:bold; font-size: 1rem;"> Duty Roster Key</p>
-						<hr style="color:#15b178;">
-						<?php $colors = Modules::run('schedules/getrosterKey'); ?>
-						<div class="col-lg-12" style="text-align:center;">
-							<p style="text-align:center; font-weight:bold; font:14rem;"></p>
-							<?php foreach ($colors as $color) { ?>
-								<button type="button" class="btn btn-sm btnkey bg-gray-dark color-pale"><?php echo $color->schedule; ?> (<?php echo $color->letter; ?>)
-								</button>
-							<?php  } ?>
 						</div>
-					</div>
-					<style>
-						.btnkey {
-							min-width: 100px;
-							;
-							color: #fff;
-							margin: 2px;
-							font-size: 11px;
-							overflow: hidden;
-						}
-
-						.tabtable {
-							zoom: 85%;
-						}
-
-						@media only screen and (max-width: 600px) {
+						<style>
 							.btnkey {
-								width: 100%;
+								min-width: 100px;
+								;
+								color: #fff;
+								margin: 2px;
+								font-size: 11px;
+								overflow: hidden;
 							}
-						}
-					</style>
-					<?php
-					?>
-					<div class="row">
-						<div class="col-md-12">
-							<img src="<?php echo base_url(); ?>assets/img/MOH.png" width="100px" style="float:left;">
+
+							.tabtable {
+								zoom: 85%;
+							}
+
+							@media only screen and (max-width: 600px) {
+								.btnkey {
+									width: 100%;
+								}
+							}
+						</style>
+						<?php
+						?>
+						<div class="row">
+							<div class="col-md-12">
+								<img src="<?php echo base_url(); ?>assets/img/MOH.png" width="100px" style="float:left;">
+							</div>
+							<div class="col-md-12" style="border-right: 0; border-left: 0; border-top: 0; margin:0 auto;">
+								<p style="text-align:center; font-weight:bold; font-size:20px;">
+									MONTHLY DUTY ROSTER REPORT FOR
+									<?php
+									echo " - " . $_SESSION['facility_name'];
+									echo "              " . date('F, Y', strtotime($year . "-" . $month));
+									?></p>
+							</div>
+							<div class="row pull-right" style="padding: 0.5rem;"> <?php echo $links; ?> </div>
 						</div>
-						<div class="col-md-12" style="border-right: 0; border-left: 0; border-top: 0; margin:0 auto;">
-							<p style="text-align:center; font-weight:bold; font-size:20px;">
-								MONTHLY DUTY ROSTER REPORT FOR
+						<div id="table" class="tabtable" style="max-width: 100%;">
+							<div class="header-row tbrow">
+								<span class="cell tbprimary"># <b id="name"></b></span>
+								<span class="cell name">Name</span>
+								<span class="cell">Position</span>
 								<?php
-								echo " - " . $_SESSION['facility_name'];
-								echo "              " . date('F, Y', strtotime($year . "-" . $month));
-								?></p>
+								$monthdays = cal_days_in_month(CAL_GREGORIAN, $month, $year); // get days in a month
+								for ($i = 1; $i < ($monthdays + 1); $i++) {
+									$dy = $i;
+									if ($i < 10) {
+										$dy = "0" . $i;
+									}
+									$wekday = $year . "-" . $month . "-" . $dy;
+									if (isWeekend($wekday) == 'yes') {
+										$color = "red";
+									} else {
+										$color = "";
+									}
+								?>
+									<span class="cell" style="padding:0px; text-align: center; border: 1px solid; background-color: <?php echo $color; ?>"><?php echo $i; ?></span>
+								<?php } ?>
+							</div>
+							<?php
+							// if beyond tenth disable editing or for other month for non system admins
+							$no = (!empty($this->uri->segment(3))) ? $this->uri->segment(3) : 0;
+							foreach ($duties as $singleduty) {
+								// print_r($singleduty);
+								$no++;
+							?>
+								<div class="table-row tbrow">
+									<input type="radio" name="expand" class="fa fa-angle-double-down trigger">
+									<span class="cell tbprimary" style="cursor:pointer;" data-label="#"><?php echo $no; ?>
+										<b id="name">. &nbsp;<span onclick="$('.trigger').click();"><?php echo $singleduty['fullname']; ?></span></b>
+									</span>
+									<span class="cell text-left name" data-label="Name"><?php echo character_limiter($singleduty['fullname'], 15); ?></span>
+									<span class="cell text-left" data-label="Position"><?php echo character_limiter($singleduty['job'], 15); ?>
+									</span>
+									<?php
+									for ($i = 1; $i < ($monthdays + 1); $i++) {
+										$state = "";
+										$date_d = $year . "-" . $month . "-" . (($i < 10) ? "0" . $i : $i);
+										$pid    = $singleduty['ihris_pid'];
+										$entry_id = $year . "-" . $month . "-" . (($i < 10) ? "0" . $i : $i) . $singleduty['ihris_pid'];
+										$duty_letter = retrieve_schedule($pid, $date_d);
+										//determine whetehr to update or insert on ajax
+										$record_type = (!empty($duty_letter)) ? "update duty" : "new duty";
+									?>
+										<span class="cell" data-label="Day<?php echo $i; ?>">
+											<?php echo $duty_letter; ?>
+										</span>
+									<?php } // end for , one that loops tds 
+									?>
+								</div>
+							<?php }
+							?>
 						</div>
 						<div class="row pull-right" style="padding: 0.5rem;"> <?php echo $links; ?> </div>
 					</div>
-					<div id="table" class="tabtable" style="max-width: 100%;">
-						<div class="header-row tbrow">
-							<span class="cell tbprimary"># <b id="name"></b></span>
-							<span class="cell name">Name</span>
-							<span class="cell">Position</span>
-							<?php
-							$monthdays = cal_days_in_month(CAL_GREGORIAN, $month, $year); // get days in a month
-							for ($i = 1; $i < ($monthdays + 1); $i++) {
-								$dy = $i;
-								if ($i < 10) {
-									$dy = "0" . $i;
-								}
-								$wekday = $year . "-" . $month . "-" . $dy;
-								if (isWeekend($wekday) == 'yes') {
-									$color = "red";
-								} else {
-									$color = "";
-								}
-							?>
-								<span class="cell" style="padding:0px; text-align: center; border: 1px solid; background-color: <?php echo $color; ?>"><?php echo $i; ?></span>
-							<?php } ?>
-						</div>
-						<?php
-						// if beyond tenth disable editing or for other month for non system admins
-						$no = (!empty($this->uri->segment(3))) ? $this->uri->segment(3) : 0;
-						foreach ($duties as $singleduty) {
-							// print_r($singleduty);
-							$no++;
-						?>
-							<div class="table-row tbrow">
-								<input type="radio" name="expand" class="fa fa-angle-double-down trigger">
-								<span class="cell tbprimary" style="cursor:pointer;" data-label="#"><?php echo $no; ?>
-									<b id="name">. &nbsp;<span onclick="$('.trigger').click();"><?php echo $singleduty['fullname']; ?></span></b>
-								</span>
-								<span class="cell text-left name" data-label="Name"><?php echo character_limiter($singleduty['fullname'], 15); ?></span>
-								<span class="cell text-left" data-label="Position"><?php echo character_limiter($singleduty['job'], 15); ?>
-								</span>
-								<?php
-								for ($i = 1; $i < ($monthdays + 1); $i++) {
-									$state = "";
-									$date_d = $year . "-" . $month . "-" . (($i < 10) ? "0" . $i : $i);
-									$pid    = $singleduty['ihris_pid'];
-									$entry_id = $year . "-" . $month . "-" . (($i < 10) ? "0" . $i : $i) . $singleduty['ihris_pid'];
-									$duty_letter = retrieve_schedule($pid, $date_d);
-									//determine whetehr to update or insert on ajax
-									$record_type = (!empty($duty_letter)) ? "update duty" : "new duty";
-								?>
-									<span class="cell" data-label="Day<?php echo $i; ?>">
-										<?php echo $duty_letter; ?>
-									</span>
-								<?php } // end for , one that loops tds 
-								?>
-							</div>
-						<?php }
-						?>
-					</div>
-					<div class="row pull-right" style="padding: 0.5rem;"> <?php echo $links; ?> </div>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
 </div>
 <script type="text/javascript">
 	var url = window.location.href;
