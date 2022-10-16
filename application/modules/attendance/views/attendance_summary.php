e5de0e9da78cd6af30822e72d7fd80c0fe4f7ea5
AGABA ANDREW <agabaandre@gmail.com>
2022-10-16 16:39:08 +0300

    roster fix

--- application/modules/attendance/views/attendance_summary.php
+++ application/modules/attendance/views/attendance_summary.php
@@ -211,7 +211,7 @@
 							$night = $roster['Night'][0]->days;
 							$r_days = $day + $eve + $night;
 							if ($r_days == 0) {
-								echo "I am here";
+								echo $_rdays = cal_days_in_month(CAL_GREGORIAN, $$userdata['month'], $userdata['year']);
 							}
 							// echo days_absent_helper($present, $r_days); 
 							?>
