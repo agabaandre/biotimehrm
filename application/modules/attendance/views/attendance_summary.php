f2147dfc4aebff07fc4dc784c1d8d37425f10240
AGABA ANDREW <agabaandre@gmail.com>
2022-10-16 16:45:21 +0300

    roster fix

--- application/modules/attendance/views/attendance_summary.php
+++ application/modules/attendance/views/attendance_summary.php
@@ -212,7 +212,7 @@
 							$r_days = $day + $eve + $night;
 							if ($r_days == 0) {
 							}
-							$r_days = cal_days_in_month(CAL_GREGORIAN, $month, $year)
+							echo $r_days = cal_days_in_month(CAL_GREGORIAN, $month, $year)
 							//echo $_rdays = cal_days_in_month(CAL_GREGORIAN, $userdata['month'], $userdata['year']);
 							// echo days_absent_helper($present, $r_days); 
 							?>
