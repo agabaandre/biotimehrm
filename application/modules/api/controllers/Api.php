17be02d39fb1f24dc70cd4e4ffad7252018022e1
Paul <paulobunga.one@gmail.com>
2022-10-17 20:46:12 +0300

    Updated Code

--- application/modules/api/controllers/Api.php
+++ application/modules/api/controllers/Api.php
@@ -1,83 +1,144 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-require APPPATH . 'libraries/REST_Controller.php';
+// Load Rest_Controller
+use chriskacerguis\RestServer\RestController;
 
-class Api extends REST_Controller
-{
-    public function __construct()
-    {
+class Api extends RestController {
+
+    public function __construct() {
         parent::__construct();
-        $this->load->model('Auth_Model', 'auth_model');
-        $this->load->model('Employee_Model', 'employee_model');
+        $this->load->model('auth_model', 'mAuth');
+        $this->load->model('employee_model', 'mEmployee');
     }
-    public function index_get()
-    {
-        echo "HRM ATTEND API";
-    }
-    public function login()
-    {
 
+    public function login_post() {
+        $username = $this->post('username');
+        $password = $this->post('password');
+
+        $result = $this->mAuth->login($username, $password);
+
+        if($result != null) {  
+            $this->response([
+                'status' => true,
+                'message' => 'Login successful',
+                'data' => [
+                    'user' => $result
+                ]
+            ], 200);
+        }
+
+        $this->response([
+            'status' => false,
+            'message' => 'Login failed'
+        ], 401);
+    }
+
+    // Get Staff List
+    public function staff_get() {
+        $facilityId = $this->get('facility_id', true);
+
+        $result = $this->mEmployee->get_staff_list($facilityId);
+        if($result != null) {
+            $this->response([
+                'status' => true,
+                'message' => 'Staff list',
+                'data' => [
+                    'staff' => $result
+                ]
+            ], 200);
+        }
+
+        $this->response([
+            'status' => false,
+            'message' => 'No staff found'
+        ], 404);
+    }
+
+    // Enroll Users
+    public function enroll_users_post() {
+        //Array of enrolled ids
+        $enrolled_ids = array();
+
+        $data = $this->post();
+       // For Each Data as a record
+        foreach($data as $record) {
+            $userRecord =  array();
+            $userRecord['entry_id'] = $record['entry_id'];
+            $userRecord['ihris_pid'] = $record['ihris_pid'];
+            $userRecord['facility_id'] = $record['facility_id'];
+            $userRecord['face_data'] = $record['face_data'];
+            $userRecord['fingerprint'] = $record['fingerprint'];
+            $userRecord['location'] = $record['location'];
+            $userRecord['card_number'] = $record['card_number'];
+            $userRecord['enroll_date'] = $record['enroll_date'];
+            $userRecord['device'] = $record['device'];
+
+            $result = $this->mEmployee->enroll($userRecord);
+
+            if($result != null) {
+                array_push($enrolled_ids, $result);
+            }
+
+            
+        }
         
+        if(count($enrolled_ids) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'Enrollment successful',
+                'data' => [
+                    'enrolled_ids' => $enrolled_ids
+                ]
+            ], 200);
+        }
+
+        $this->response([
+            'status' => false,
+            'message' => 'Enrollment failed'
+        ], 404);
     }
 
-    public function clock_user($data)
-    {
-        //
-        $userdata = $this->input->post();
-        $result = $this->employee_model->clock_user($userdata);
-        echo json_encode($result);
+    public function clock_users_post() {
+        $data = $this->post();
+        $clocked_ids = array();
+
+        foreach($data as $record) {
+            $userRecord =  array();
+            $userRecord['entry_id'] = $record['entry_id'];
+            $userRecord['ihris_pid'] = $record['ihris_pid'];
+            $userRecord['facility_id'] = $record['facility_id'];
+            $userRecord['time_in'] = $record['time_in'];
+            $userRecord['time_out'] = $record['time_out'];
+            $userRecord['date'] = $record['date'];
+            $userRecord['status'] = $record['status'];
+            // Location, Source, Facility
+            $userRecord['location'] = $record['location'];
+            $userRecord['source'] = $record['source'];
+            $userRecord['facility'] = $record['facility'];
+
+            $result = $this->mEmployee->clock($userRecord);
+
+            if($result != null) {
+                array_push($clocked_ids, $result);
+            }
+        }
+
+        if(count($clocked_ids) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'Clocking successful',
+                'data' => [
+                    'clocked_ids' => $clocked_ids
+                ]
+            ], 200);
+        }
+
+        $this->response([
+            'status' => false,
+            'message' => 'Clocking failed'
+        ], 401);
     }
 
-    public function clock_history($facility)
-    {
-        // to download
-    }
-
-    public function enroll_user($userdata)
-    {
-        $userdata = $this->input->post();
-        $result = $this->employee_model->enroll_user($userdata);
-        echo json_encode($result);
-    }
-    public function upload_resources($userdata)
-    {
-        //image ai data set
-    }
-    public function download_resources()
-    {
-        //image ai data set
-
-    }
-    public function get_device_status()
-    {
-
-        //Device should call the server with the location, Facility ID and Time Stamp sERIAL NUMBER
-    }
-    public function receive_notfications($facility, $all)
-    {
-
-        //Device should call the server with the location, Facility ID and Time Stamp sERIAL NUMBER
-    }
-
-    public function enrolled_users($facilityId)
-    {
-        $result = $this->employee_model->get_enrolled_employees(urldecode($facilityId));
-        echo json_encode($result);
-    }
-
-    public function employees($facility)
-    {
-        //provides an employee list for enrollment
-    }
-//individual logins int the app
-    public function sendRequest()
-    {
-        //File upload
-
-    }
-    public function getRequest_Status($user)
-    {
-        //order by date
-    }
+    
 }
--- application/modules/api/models/Auth_Model.php
+++ application/modules/api/models/Auth_Model.php
@@ -3,10 +3,51 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth_model extends CI_Model
 {
-    public function validate_login($userdata)
+    // Constructor
+    public function __construct()
     {
-     //join users with user_facilities for sitautions where a user has more than one facility.
-
-      
+        parent::__construct();
+        $this->load->library('Auth_Token', 'auth_token');
     }
+    
+    // Login
+    public function login($username, $password) {
+        $this->db->select('user_id, email, username, name, role, status, ihris_pid, facility_id, facility, department_id, department, district_id, district');
+        $this->db->where("username", $username);
+        $this->db->where("password", md5($password));
+        $this->db->from("user");
+        $query = $this->db->get();
+        if ($query->num_rows() > 0) {
+            return $query->row();
+        } else {
+            return null;
+        }
+    } 
+    public function generateToken($data) {
+        $data_token['user_id'] = $data['user_id'];
+        $data_token['username'] = $data['username'];
+        $data_token['name'] = $data['name'];
+        $data_token['role'] = $data['role'];
+        $user_token = $this->auth_token->generateToken($data_token);
+        return $user_token;
+    } 
+
+    public function validateToken($token) {
+        
+        $is_valid_token = $this->auth_token->validateToken($token);
+        if (!$is_valid_token) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Get Staff List
+    public function get_staff_list() {
+        // Get staff list from fingerprints table
+        $this->db->select('*');
+        $this->db->from('ihrisdata');
+
+    }
+
 }
