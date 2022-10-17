45d60105a29cd898b0ad97719ebb6dd9ab3dbf9e
AGABA ANDREW <agabaandre@gmail.com>
2022-10-17 23:58:58 +0300

    update of csv helper

--- application/modules/reports/controllers/Reports.php
+++ application/modules/reports/controllers/Reports.php
@@ -373,11 +373,11 @@ class Reports extends MX_Controller
 			$absent = $month_days - ($row->P + $row->O + $row->R + $row->L);
 			$abrate = number_format(($absent / $month_days), 1) * 100;
 
-			$row =  [$row->fullname, $row->district, $row->facility_name, $row->P, $row->O, $row->R, $row->L, $absent, $abrate];
+			$row =  [$row->fullname, $row->district, $row->facility_name, $row->duty_date, $row->P, $row->O, $row->R, $row->L, $absent, $abrate];
 
 			array_push($exportable, $row);
 		}
 
-		render_csv_data($exportable, "attendance_aggregates_" . time());
+		render_csv_data($exportable, "person_attendance_all" . time(), false);
 	}
 }
