a92bc8fbc08de86f81cbb1a6aa12f104d7b779b4
Agaba Andre <agabaandre@gmail.com>
2022-11-10 00:59:47 +0300

    Update Apiauth_model.php

--- application/modules/api/models/Apiauth_model.php
+++ application/modules/api/models/Apiauth_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Auth_model extends CI_Model
+class Apiauth_model extends CI_Model
 {
     // Constructor
     public function __construct()
