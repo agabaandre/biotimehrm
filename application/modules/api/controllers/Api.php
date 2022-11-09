1993a37f94e9642167afefaaa9b6cce1c6c04cb0
Agaba Andre <agabaandre@gmail.com>
2022-11-10 00:54:15 +0300

    Update Api.php

--- application/modules/api/controllers/Api.php
+++ application/modules/api/controllers/Api.php
@@ -15,8 +15,8 @@ class Api extends RestController
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('Apiauth_model', 'mAuth');
-        $this->load->model('Apiemployee_model', 'mEmployee');
+        $this->load->model('apiauth_model', 'mAuth');
+        $this->load->model('apiemployee_model', 'mEmployee');
     }
 
     public function validateRequest()
