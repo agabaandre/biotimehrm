cd59e97d86a82898ac3e38352185af342fd146ab
Agaba Andre <agabaandre@gmail.com>
2022-11-10 00:45:30 +0300

    fix model load

--- application/modules/api/controllers/Api.php
+++ application/modules/api/controllers/Api.php
@@ -15,8 +15,8 @@ class Api extends RestController
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('auth_model', 'mAuth');
-        $this->load->model('employee_model', 'mEmployee');
+        $this->load->model('Auth_model', 'mAuth');
+        $this->load->model('Employee_model', 'mEmployee');
     }
 
     public function validateRequest()
