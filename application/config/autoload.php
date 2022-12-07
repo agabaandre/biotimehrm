79a682ace999a69f10da2e8052cc4332f687ab95
Agaba Andrew <agabaandre@gmail.com>
2022-12-07 21:20:56 +0300

    changed users

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session','pagination');
+$autoload['libraries'] = array('database', 'session', 'pagination', 'ArgonHash');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'absent', 'rosta', 'text', 'custom','attendance');
+$autoload['helper'] = array('url', 'absent', 'rosta', 'text', 'custom', 'attendance');
 
 /*
 | -------------------------------------------------------------------
