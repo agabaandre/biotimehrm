6e91c8c5f019aaec873ae671a3e735a539eefc43
Agaba Andrew <agabaandre@gmail.com>
2022-12-13 12:50:22 +0300

    dashboard

--- application/modules/biotimejobs/controllers/Biotimejobs.php
+++ application/modules/biotimejobs/controllers/Biotimejobs.php
@@ -207,7 +207,7 @@ class Biotimejobs extends MX_Controller
 
         //if las sync is empty
         $sdate = "2022-10-14 00:00:00";
-        $edate = "2022-12-06 00:00:00";
+        $edate = "2022-10-16 00:00:00";
 
         // $sdate = date("Y-m-d H:i:s", strtotime("-12 hours"));
         $query = array(
