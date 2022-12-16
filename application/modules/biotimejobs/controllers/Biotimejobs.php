1493e39c50051e18c0e31b84ca70effdbe53a899
Agaba Andrew <agabaandre@gmail.com>
2022-12-16 08:57:34 +0300

    fix person _attendance

--- application/modules/biotimejobs/controllers/Biotimejobs.php
+++ application/modules/biotimejobs/controllers/Biotimejobs.php
@@ -206,8 +206,8 @@ class Biotimejobs extends MX_Controller
         }
 
         //if las sync is empty
-        $sdate = "2022-10-30 00:00:00";
-        $edate = "2022-11-15 00:00:00";
+        $sdate = "2022-10-21 00:00:00";
+        $edate = "2022-11-01 00:00:00";
 
         // $sdate = date("Y-m-d H:i:s", strtotime("-12 hours"));
         $query = array(
