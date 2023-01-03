939cbf415ffd9c921f1344d564faa804a4a87584
AGABA ANDREW <agabaandre@gmail.com>
2023-01-03 06:27:59 +0300

    jobs fix

--- application/modules/biotimejobs/controllers/Biotimejobs.php
+++ application/modules/biotimejobs/controllers/Biotimejobs.php
@@ -221,11 +221,11 @@ class Biotimejobs extends MX_Controller
 
 
         //if las sync is empty
-         // $sdate = "2023- 00:00:00";
-          $sdate = "2023-01-01 23:59:00";
-          $edate = "2023-01-02 23:59:00";
+    
+         // $sdate = "2023-01-01 23:59:00";
+        //  $edate = "2023-01-02 23:59:00";
 
-        //$sdate = date("Y-m-d H:i:s", strtotime("-12 hours"));
+        $sdate = date("Y-m-d H:i:s", strtotime("-12 hours"));
         $query = array(
             'page' => $page, 'start_time' => $sdate,
             'end_time' => $edate,
