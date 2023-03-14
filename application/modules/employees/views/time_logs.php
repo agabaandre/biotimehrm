33d52dc875fac28d66b92789e6492803950c12e6
AGABA ANDREW <agabaandre@gmail.com>
2023-03-14 18:02:17 +0300

    new things

--- application/modules/employees/views/time_logs.php
+++ application/modules/employees/views/time_logs.php
@@ -102,7 +102,7 @@
             <td><?php echo $timelog->job; ?></td>
             <td><?php echo date('j F,Y', strtotime($timelog->date)); ?></td>
             <td><?php echo date('H:i:s', strtotime($time_in = $timelog->time_in)); ?></td>
-            ` <td><?php if (!empty($time_out = $timelog->time_out)) {
+            <td><?php if (!empty($time_out = $timelog->time_out)) {
                     echo date('H:i:s', strtotime($time_out = $timelog->time_out));
                   } ?></td>
             <td><?php
