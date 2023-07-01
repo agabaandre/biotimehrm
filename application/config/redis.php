fdcfa6852f873437a304bcdd5af052a88db6c9dc
AGABA ANDREW <agabaandre@gmail.com>
2023-07-01 21:28:48 +0300

    added redis

---
+++ application/config/redis.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$config['redis_slave']['host'] = '127.0.0.1';		// IP address or host
+$config['redis_slave']['port'] = '6379';			// Default Redis port is 6379
+$config['redis_slave']['password'] = '';			
+
+?>
