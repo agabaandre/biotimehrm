378a361359ce25737e4985682dc4fb877e2f97d8
AGABA ANDREW <agabaandre@gmail.com>
2023-07-25 22:14:15 +0300

    jobs

--- application/modules/biotimejobs/controllers/Biotimejobs.php
+++ application/modules/biotimejobs/controllers/Biotimejobs.php
@@ -526,7 +526,7 @@ class Biotimejobs extends MX_Controller
             array_push($j, $data);
            
         }
-        dd($j);
+        dd($response->data);
 
         $message = $this->biotimejobs_mdl->save_jobs($j);
         $process = 8;
