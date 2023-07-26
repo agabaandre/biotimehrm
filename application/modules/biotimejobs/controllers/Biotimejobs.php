54d1265bc0a612955c1fb7035a244d6a2492da47
AGABA ANDREW <agabaandre@gmail.com>
2023-07-26 16:22:20 +0300

    finalised transfers

--- application/modules/biotimejobs/controllers/Biotimejobs.php
+++ application/modules/biotimejobs/controllers/Biotimejobs.php
@@ -670,7 +670,7 @@ class Biotimejobs extends MX_Controller
                 // array_push($rows, $data);
             }
         }
-        dd($data);
+       // dd($data);
 
         $process = 7;
         $method = "bioitimejobs/biotime_employees";
