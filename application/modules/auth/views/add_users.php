90507257cacf6ff06b6a472aaab44133d2b4da20
AGABA ANDREW <agabaandre@gmail.com>
2023-08-16 17:46:33 +0300

    csv

--- application/modules/auth/views/add_users.php
+++ application/modules/auth/views/add_users.php
@@ -18,7 +18,7 @@ $variables = Modules::run("svariables/getSettings");
     <!-- general form elements disabled -->
     <div class="card card-default">
       <div class="card-header">
-        <h3 class="card-title">Add User</h3>
+        <h3 class="card-title">Add User (Use for Non Health Professionals Only)</h3>
       </div>
       <!-- /.card-header -->
       <div class="card-body">
