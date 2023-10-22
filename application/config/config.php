154301207910b36418acddb77643df23688e07cb
AGABA ANDREW <agabaandre@gmail.com>
2023-10-22 19:57:57 +0300

    upgrade dashboard

--- application/config/config.php
+++ application/config/config.php
@@ -398,7 +398,6 @@ $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'attendance';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = APPPATH . 'cache/temp/';
-$config['sess_save_path'] = FALSE;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
