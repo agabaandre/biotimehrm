e8405937732ee3f4087df61c05e94dc658d7a1d3
AGABA ANDREW <agabaandre@gmail.com>
2023-10-22 19:14:01 +0300

    upgrade dashboard

--- application/modules/dashboard/views/main_dashboard.php
+++ application/modules/dashboard/views/main_dashboard.php
@@ -1,31 +1,39 @@
 <div class="table-responsive" id="staff-container">
 	<!-- Table content will be dynamically loaded here -->
 </div>
+<div id="loading-indicator" style="display: none;" style="margin:0 auto;">
+	<img src="<?php echo base_url()?>assets/loader2.png">
+</div>
 <script>
-	
-		function fetchDataAndUpdate() {
-			$.ajax({
-				url: '<?php echo base_url() ?>dashboard/get_dashboard',
-				type: 'GET',
-				dataType: 'json', // Expecting JSON response
-				success: function(response) {
-					if (response.html) {
-						// Use html() to replace the content
-						$('#staff-container').html(response.html);
-					} else {
-						console.error('No HTML content found in the response.');
-					}
-				},
-				error: function(xhr, status, error) {
-					console.error('AJAX Error:', status, error);
+	function fetchDataAndUpdate() {
+		// Show the loading indicator while fetching data
+		$('#loading-indicator').show();
+
+		$.ajax({
+			url: '<?php echo base_url() ?>dashboard/get_dashboard',
+			type: 'GET',
+			dataType: 'json', // Expecting JSON response
+			success: function(response) {
+				if (response.html) {
+					// Use html() to replace the content
+					$('#staff-container').html(response.html);
+				} else {
+					console.error('No HTML content found in the response.');
 				}
-			});
-		}
+			},
+			error: function(xhr, status, error) {
+				console.error('AJAX Error:', status, error);
+			},
+			complete: function() {
+				// Hide the loading indicator when the request is complete
+				$('#loading-indicator').hide();
+			}
+		});
+	}
 
-		// Fetch data immediately when the page loads
-		fetchDataAndUpdate();
+	// Fetch data immediately when the page loads
+	fetchDataAndUpdate();
 
-		// Update data every 30 minutes (30 * 60 * 1000 milliseconds)
-		setInterval(fetchDataAndUpdate, 30 * 60 * 1000);
-
+	// Update data every 30 minutes (30 * 60 * 1000 milliseconds)
+	setInterval(fetchDataAndUpdate, 30 * 60 * 1000);
 </script>
