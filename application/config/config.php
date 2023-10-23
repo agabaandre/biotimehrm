dd03ee460e37ab298ae33033c133b85490306b1e
unknown <MOH-ICT-BOO8-09@MOH-ICT-B008-09.health.go.ug>
2023-10-23 22:48:36 +0300

    fixed composer loading issues

--- application/config/config.php
+++ application/config/config.php
@@ -68,7 +68,7 @@ $config['index_page'] = 'index.php';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']  = 'REQUEST_URI';
+$config['uri_protocol'] = 'REQUEST_URI';
 
 /*
 |--------------------------------------------------------------------------
@@ -92,7 +92,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']  = 'english';
+$config['language'] = 'english';
 
 /*
 |--------------------------------------------------------------------------
@@ -394,21 +394,21 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'attendance';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = APPPATH . 'cache/temp/';
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = FALSE;
-
-// $config['sess_driver'] = 'database';
-// $config['sess_cookie_name'] = 'attendances';
+// $config['sess_driver'] = 'files';
+// $config['sess_cookie_name'] = 'attendance';
 // $config['sess_expiration'] = 7200;
-// $config['sess_save_path'] = 'access_sessions';
+// $config['sess_save_path'] = APPPATH . 'cache/temp/';
 // $config['sess_match_ip'] = FALSE;
 // $config['sess_time_to_update'] = 300;
 // $config['sess_regenerate_destroy'] = FALSE;
+
+$config['sess_driver'] = 'database';
+$config['sess_cookie_name'] = 'attendances';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = 'access_sessions';
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
+$config['sess_regenerate_destroy'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
@@ -425,11 +425,11 @@ $config['sess_regenerate_destroy'] = FALSE;
 |       'cookie_httponly') will also affect sessions.
 |
 */
-$config['cookie_prefix']  = '';
-$config['cookie_domain']  = '';
-$config['cookie_path']    = '/';
-$config['cookie_secure']  = FALSE;
-$config['cookie_httponly']   = FALSE;
+$config['cookie_prefix'] = '';
+$config['cookie_domain'] = '';
+$config['cookie_path'] = '/';
+$config['cookie_secure'] = FALSE;
+$config['cookie_httponly'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
