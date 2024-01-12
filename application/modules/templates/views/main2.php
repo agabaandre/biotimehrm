88ed6715a5ac8b4490e169b1ce89611dcfd72e51
Agaba Andrew <agabaandre@gmail.com>
2024-01-12 22:38:57 +0300

    privacy

--- application/modules/templates/views/main2.php
+++ application/modules/templates/views/main2.php
@@ -5,11 +5,11 @@
 
 ?>
 <!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
+
 <?php
     $this->load->view($module . "/" . $view);
 ?>
-</div>
+
 <!-- /.content-wrapper -->
 <?php
     require_once("includes/footer.php");
