e1cc5335a5ec6c80a4b7c081bf6eff9ac34b047c
Agaba Andrew <agabaandre@gmail.com>
2024-01-12 22:35:55 +0300

    privacy

--- application/modules/templates/views/main2.php
+++ application/modules/templates/views/main2.php
@@ -1,6 +1,7 @@
 <?php
     date_default_timezone_set('Africa/Kampala');
     require_once("includes/header.php");
+    require_once("includes/navtop.php");
 
 ?>
 <!-- Content Wrapper. Contains page content -->
