a0249e65efa55f4e2c2820e62571048edc9ef213
Agaba Andrew <agabaandre@gmail.com>
2024-01-13 04:22:49 +0300

    privacy

--- application/modules/lists/models/Districts_mdl.php
+++ application/modules/lists/models/Districts_mdl.php
@@ -25,7 +25,7 @@ class Districts_mdl extends CI_Model {
 	{
         
 		 $permissions = $this->session->userdata('permissions');
-		 dd($permissions);
+		 dd(array_values($permissions));
 		if (!(in_array('46', $permissions))) {
 
 			$district_id = $this->session->userdata('district_id');
