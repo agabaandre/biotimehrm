160a84b3de684109706a00e1bb87067d974852f5
Agaba Andrew <agabaandre@gmail.com>
2024-04-10 20:41:36 +0300

    biotime_jobs

--- application/modules/biotimejobs/controllers/Biotimejobs.php
+++ application/modules/biotimejobs/controllers/Biotimejobs.php
@@ -913,7 +913,7 @@ class Biotimejobs extends MX_Controller
         $data = array('process_id' => $process, 'process' => $method, 'status' => $status);
         $this->db->replace("cronjob_register", $data);
     }
-    public function fetch_time_history($start_date = '2023-01-01', $end_date = '2023-03-02', $empcode=FALSE, $terminal_sn=FALSE)
+    public function fetch_time_history($start_date = '2023-01-01', $end_date = '2023-03-05', $empcode=FALSE, $terminal_sn=FALSE)
     {
         ignore_user_abort(true);
         ini_set('max_execution_time', 0);
