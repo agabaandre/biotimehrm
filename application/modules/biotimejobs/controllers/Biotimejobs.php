12221e6f066540c6f1197c2affa8ba69258d8e2b
Agaba Andrew <agabaandre@gmail.com>
2024-04-29 18:37:07 +0300

    new job to fetch attendance logs from postgres

--- application/modules/biotimejobs/controllers/Biotimejobs.php
+++ application/modules/biotimejobs/controllers/Biotimejobs.php
@@ -1017,4 +1017,11 @@ class Biotimejobs extends MX_Controller
 
     }
 
+    public function daily_logs($ihris_pid,$date){
+        $this->db->where("date", "$date");
+        $this->db->where("ihris_pid","$ihris_pid");
+       $data =  $this->db->get('clk_log')->row();
+    echo json_encode($data);
+    }
+
 }
