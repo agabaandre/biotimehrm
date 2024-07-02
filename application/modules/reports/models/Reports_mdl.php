3dc7271fcbff422588c28440305f04ddefd232cd
Agaba Andrew <agabaandre@gmail.com>
2024-07-02 17:33:44 +0300

    api

--- application/modules/reports/models/Reports_mdl.php
+++ application/modules/reports/models/Reports_mdl.php
@@ -242,7 +242,7 @@ class Reports_mdl extends CI_Model
 		if ($limit){
 			$this->db->limit($limit, $start);
 		}
-		if ($filters->facility_name === '') {
+		if ($filters->facility_name == null) {
 			$facility_id = $_SESSION['facility'];
 			$this->db->where("facility_id", "$facility_id");
 		}
