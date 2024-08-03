a5efd3bb7b6e44e73fc12df41f4ae6ea0d71fa34
Agaba Andrew <agabaandre@gmail.com>
2024-08-03 14:47:45 +0300

    fhir post

--- application/modules/biotimejobs/controllers/Biotimejobs.php
+++ application/modules/biotimejobs/controllers/Biotimejobs.php
@@ -1508,7 +1508,7 @@ class Biotimejobs extends MX_Controller
         $valid_range = '2024-07';
         $district='MBALE';
         $body = $this->attendance_data('TRUE',$valid_range,$district);
-dd($body);
+        dd($body);
         $http = new HttpUtils();
 
 
