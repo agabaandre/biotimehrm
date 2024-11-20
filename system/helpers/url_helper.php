c552c7dc71bc5ea7f617232e010834e2728558d4
Agaba Andre <agabaandre@gmail.com>
2024-11-20 15:59:43 +0300

    Update url_helper.php

--- system/helpers/url_helper.php
+++ system/helpers/url_helper.php
@@ -530,40 +530,42 @@ if ( ! function_exists('redirect'))
 	 * @return	void
 	 */
 	function redirect($uri = '', $method = 'auto', $code = NULL)
-	{
-		if ( ! preg_match('#^(\w+:)?//#i', $uri))
-		{
-			$uri = site_url($uri);
-		}
+{
+    if (headers_sent($file, $line)) {
+        throw new Exception("Headers already sent in $file on line $line. Cannot redirect to $uri.");
+    }
 
-		// IIS environment likely? Use 'refresh' for better compatibility
-		if ($method === 'auto' && isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS') !== FALSE)
-		{
-			$method = 'refresh';
-		}
-		elseif ($method !== 'refresh' && (empty($code) OR ! is_numeric($code)))
-		{
-			if (isset($_SERVER['SERVER_PROTOCOL'], $_SERVER['REQUEST_METHOD']) && $_SERVER['SERVER_PROTOCOL'] === 'HTTP/1.1')
-			{
-				$code = ($_SERVER['REQUEST_METHOD'] !== 'GET')
-					? 303	// reference: http://en.wikipedia.org/wiki/Post/Redirect/Get
-					: 307;
-			}
-			else
-			{
-				$code = 302;
-			}
-		}
+    if ( ! preg_match('#^(\w+:)?//#i', $uri))
+    {
+        $uri = site_url($uri);
+    }
 
-		switch ($method)
-		{
-			case 'refresh':
-				header('Refresh:0;url='.$uri);
-				break;
-			default:
-				header('Location: '.$uri, TRUE, $code);
-				break;
-		}
-		exit;
-	}
+    if ($method === 'auto' && isset($_SERVER['SERVER_SOFTWARE']) && strpos($_SERVER['SERVER_SOFTWARE'], 'Microsoft-IIS') !== FALSE)
+    {
+        $method = 'refresh';
+    }
+    elseif ($method !== 'refresh' && (empty($code) OR ! is_numeric($code)))
+    {
+        if (isset($_SERVER['SERVER_PROTOCOL'], $_SERVER['REQUEST_METHOD']) && $_SERVER['SERVER_PROTOCOL'] === 'HTTP/1.1')
+        {
+            $code = ($_SERVER['REQUEST_METHOD'] !== 'GET') ? 303 : 307;
+        }
+        else
+        {
+            $code = 302;
+        }
+    }
+
+    switch ($method)
+    {
+        case 'refresh':
+            header('Refresh:0;url='.$uri);
+            break;
+        default:
+            header('Location: '.$uri, TRUE, $code);
+            break;
+    }
+    exit;
+}
+
 }
