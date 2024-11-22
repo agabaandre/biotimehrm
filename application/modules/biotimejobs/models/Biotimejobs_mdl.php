a1ab22cc0bf4544ed5bbdf564e38118857fd891b
Agaba Andrew <agabaandre@gmail.com>
2024-11-22 05:02:52 +0300

    pg connection changed

--- application/modules/biotimejobs/models/Biotimejobs_mdl.php
+++ application/modules/biotimejobs/models/Biotimejobs_mdl.php
@@ -226,6 +226,7 @@ class Biotimejobs_mdl extends CI_Model
     
         // Fetch all rows as associative arrays
         $rows = pg_fetch_all($result);
+        dd($rows);
     
         // Close PostgreSQL connection
         pg_close($pg_conn);
